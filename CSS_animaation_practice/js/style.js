10f1886655daebdeb486577f891c3d37493ffdfd
issanna <72988935+issanna@users.noreply.github.com>
2020-12-02 18:51:31 +0200

    Add files via upload

---
+++ CSS_animaation_practice/js/style.js
@@ -0,0 +1,15 @@
+buttonOne.onclick=function(){
+	form.classList.toggle('active');
+}
+buttonTwo.onclick=function(){
+	form.classList.toggle('active');
+}
+buttonThree.onclick=function(){
+	form.classList.toggle('active');
+}
+buttonFour.onclick=function(){
+	form.classList.toggle('active');
+}
+document.querySelectorAll('.button').onclick=function(){
+	
+}
