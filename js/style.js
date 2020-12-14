fbc1f479fffa74c6e926a998e4d0710f19649d34
issanna <72988935+issanna@users.noreply.github.com>
2020-12-14 17:20:50 +0200

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,10 @@
+
+let icon = document.getElementById('icon');
+let menu = document.getElementById('menu');
+
+icon.onclick=function(){
+	menu.classList.toggle('active');
+	icon.classList.toggle('active');
+	console.log(test);
+}
+
